6b3f316651f0a2c118c8d638fc76e8caba3d6367
bala <codebala5@gmail.com>
2025-08-31 16:49:27 +0800

    Array update

---
+++ Arrays/array1.py
@@ -0,0 +1,17 @@
+def runningSum(nums):
+    result = [nums[0]]
+
+    for i in range(1, len(nums)):
+        result.append(result[i - 1] + nums[i])
+
+    return result
+
+
+if __name__ == "__main__":
+    # Example usage:
+    input_nums = [1, 2, 3, 4]
+    output_sum = runningSum(input_nums)
+    print(f"The running sum of {input_nums} is {output_sum}")
+
+# Answer :
+# The running sum of [1, 2, 3, 4] is [1, 3, 6, 10]
