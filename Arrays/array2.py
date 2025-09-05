058c9024c70346d5f7f3c35a00e2b70e72847033
bala <codebala5@gmail.com>
2025-09-05 21:16:44 +0800

    Recheck

--- Arrays/array2.py
+++ Arrays/array2.py
@@ -1,6 +1,11 @@
 def runningSum(nums):
     result = [nums[0]]
 
+    for i in range(1, len(nums)):
+        result.append(result[i - 1] + nums[i])
+
+    return result
+
 
 if __name__ == "__main__":
     # Example usage:
